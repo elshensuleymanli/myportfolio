a693cb56d5d427fae90128809e054c7c1112ae2d
elshensuleymanli <94218490+elshensuleymanli@users.noreply.github.com>
2022-01-20 16:45:17 +0400

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,15 @@
+
+function menuOpen() {
+        
+    document.getElementById("side").style.width = "250px";
+    document.getElementById("menu").style.marginLeft = "250px";
+    
+}
+
+ function menuClose() {
+    
+    document.getElementById("side").style.width = "0px";
+    document.getElementById("menu").style.marginLeft = "0px";
+
+ }
+ 
